a43a5e727a7435b950acf07f6d19497fdbea6205
kaelzhang <i@kael.me>
2013-10-08 17:04:21 +0800

    add test cases

---
+++ test/checker.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var checker = require('../');
+var expect = require('chai').expect;
+
+describe("._parseSchema()", function(){
+    it("validator is always an array", function(){
+        var parsed = checker()._parseSchema({
+            a: {}
+        });
+
+        expect(parsed.a.validator).to.deep.equal([]);
+    });
+
+    it("setter is always an array", function(){
+        var parsed = checker()._parseSchema({
+            a: {}
+        });
+
+        expect(parsed.a.setter).to.deep.equal([]);
+    });
+
+    it("regex", function(){
+        var parsed = checker()._parseSchema({
+            a: {
+                validator: /abc/
+            }
+        });
+
+        var validator = parsed.a.validator[0];
+
+        expect(validator.length).to.equal(1);
+        expect(validator('aabcde')).to.equal(true);
+    });
+});
+
+describe(".check()", function(){
+    it("complex", function(done){
+        var schema = {
+            a: {
+                default: 'abc',
+
+                setter: function(v){
+                    return v + '123'
+                }
+            },
+
+            b: {
+                default: 'b'
+            }
+        }
+
+        var c = checker(schema);
+
+        c.check({}, function(err, value, detail){
+            expect(value.a).to.equal('abc123');
+            expect(value.b).to.equal('b');
+            expect(detail.a.is_default).to.equal(true);
+            expect(detail.b.is_default).to.equal(true);
+
+            done();
+        });
+    });
+});
