d9ccde2988c23641f08cce52ca4d8762f40ca639
kaelzhang <i@kael.me>
2014-08-12 16:05:58 +0800

    adds skema.parse;

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Skema = require('./lib/skema');
+var parser = require('./lib/parser');
 
 function skema (options) {
   options || (options = {});
@@ -11,3 +12,4 @@ function skema (options) {
 
 module.exports = skema;
 skema.Skema = Skema;
+skema.parse = parser.parse;
